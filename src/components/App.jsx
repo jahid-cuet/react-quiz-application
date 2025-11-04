04887256ca077a48ef2d5f5f9c069ea7d02bc422
Md.Jahid Hasan <jahidhasan5803733@gmail.com>
2025-11-04 19:48:11 +0600

    Result-page-done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,19 @@
 import "../styles/App.css";
 import Layout from "./Layout";
-// import Home from "./pages/Home";
-// import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Quiz from "./pages/Quiz";
-// import Signup from "./pages/Signup";
+import Result from "./pages/Result";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <Layout>
-      {/* <Home />
+      <Home />
       <Signup />
-      <Login /> */}
+      <Login />
       <Quiz />
+      <Result />
     </Layout>
   );
 }
