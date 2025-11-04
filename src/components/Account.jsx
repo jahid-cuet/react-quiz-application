24ac8e633182740b4e61946e5b5befd179fd871d
Md.Jahid Hasan <jahidhasan5803733@gmail.com>
2025-11-04 20:15:16 +0600

    route-setup-done

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,13 +1,17 @@
+import { Link } from "react-router-dom";
 import classes from "../styles/Account.module.css";
-
 export default function Account() {
   return (
     <div className={classes.account}>
       <span className="material-icons-outlined" title="Account">
         account_circle
       </span>
-      <a href="signup.html">Signup</a>
-      {/* <span class="material-icons-outlined" title="Logout"> logout </span>  */}
+      <Link to="/signup">Signup</Link>
+      <Link to="/login">Login</Link>
+      <span class="material-icons-outlined" title="Logout">
+        {" "}
+        logout{" "}
+      </span>
     </div>
   );
 }
